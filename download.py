069f617a8cca13f03f26d9dc48c2f4b33cc106aa
remmadlog <remmadlog@outlook.com>
2025-04-19 15:36:18 +0200

    added file: use for downloading anime_id files (not season data)

---
+++ download.py
@@ -0,0 +1,18 @@
+"""
+This file is used to download all anime_id driven data using api.jikan
+!!!THIS MIGHT TAKE A WHILE!!!
+    -- we do about 1 request per second (slower!)
+    -- for each mal_id/anime_id we do 6 requests (["full", "characters", "staff", "episodes", "forum", "statistics"])
+    -- we have about 25200 ids
+    -->best case: 42h
+        -- this is unrealistic! we may get stuck or get a Timeout...
+        -- take your time and maybe reduce the range
+"""
+
+from supply_module import *
+
+for year in range(1970,2025):
+    print("---------------------------------------------------------")
+    print(str(year) + "-----------------------------------------------------")
+    print("---------------------------------------------------------")
+    download_anime_year(year, anime_type="all")
